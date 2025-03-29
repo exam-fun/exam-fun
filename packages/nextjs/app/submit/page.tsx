89f2ec4dbea9a6fbc73a8002be0fa16e27f4c5d6
Archer <archer@aimoverse.xyz>
2025-03-29 20:41:24 +0800

    add basic structure

---
+++ packages/nextjs/app/submit/page.tsx
@@ -0,0 +1,7 @@
+export default function Submit() {
+    return (
+        <div>
+            <h1>Submit</h1>
+        </div>
+    );
+}
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -22,6 +22,20 @@ export const menuLinks: HeaderMenuLink[] = [
   },
 
   {
+    label: "Register",
+    href: "/register",
+  },
+
+  {
+    label: "Submit",
+    href: "/submit",
+  },
+
+  {
+    label: "Buy",
+    href: "/buy",
+  },
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
@@ -40,9 +54,8 @@ export const HeaderMenuLinks = () => {
             <Link
               href={href}
               passHref
-              className={`${
-                isActive ? "bg-secondary shadow-md" : ""
-              } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
+              className={`${isActive ? "bg-secondary shadow-md" : ""
+                } hover:bg-secondary hover:shadow-md focus:!bg-secondary active:!text-neutral py-1.5 px-3 text-sm rounded-full gap-2 grid grid-flow-col`}
             >
               {icon}
               <span>{label}</span>
