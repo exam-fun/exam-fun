2364abc1fc3ebed1e7da4b33f889ec1bb45a7a4f
Archer <archer@aimoverse.xyz>
2025-03-30 23:48:11 +0800

    move buy into user/[id]

--- packages/nextjs/app/user/[id]/page.tsx
+++ packages/nextjs/app/user/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function Buy() {
+export default function UserDetail({ params }: { params: { id: string } }) {
   const [buyAmount, setBuyAmount] = useState<string>("100");
   const [sellAmount, setSellAmount] = useState<string>("");
 
@@ -16,7 +16,7 @@ export default function Buy() {
             <div className="w-12 h-12 rounded-full bg-gray-200">{/* 头像占位 */}</div>
             <div className="flex flex-col gap-2">
               <div className="flex gap-2">
-                <div className="border rounded px-10 py-1">ID</div>
+                <div className="border rounded px-10 py-1">{params.id}</div>
                 <div className="border rounded px-10 py-1">代币名称</div>
               </div>
               <div className="border rounded px-20 py-1 ">链上地址</div>
